26cda80c0ea0ea418541aaee510161d789bc75f7
Mikael Marques de Araújo <mikaeldev760@gmail.com>
2025-01-12 17:59:10 -0300

    more-resolutions

--- api-express/app.js
+++ api-express/app.js
@@ -41,7 +41,7 @@ app.use(function(err, req, res) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
