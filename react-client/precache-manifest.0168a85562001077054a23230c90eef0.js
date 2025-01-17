f38f0859d3c142501ee1769181efc023674bddd6
Mikael Marques de Araújo <mikaeldev760@gmail.com>
2025-01-17 13:38:51 -0300

    deploy-to-gh-pages

---
+++ react-client/precache-manifest.0168a85562001077054a23230c90eef0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8d56f3e39a61074dec45cf31f864ad",
+    "url": "/js-notes/index.html"
+  },
+  {
+    "revision": "ad8f67c10c374a4d5928",
+    "url": "/js-notes/static/css/2.27b267f7.chunk.css"
+  },
+  {
+    "revision": "6bb2a4015cbf242ad2ad",
+    "url": "/js-notes/static/css/main.e04ea1de.chunk.css"
+  },
+  {
+    "revision": "ad8f67c10c374a4d5928",
+    "url": "/js-notes/static/js/2.1d011129.chunk.js"
+  },
+  {
+    "revision": "677d5fccc7ca819687c2e64468263032",
+    "url": "/js-notes/static/js/2.1d011129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1f8bbfa8916e73b6f9",
+    "url": "/js-notes/static/js/3.91b14993.chunk.js"
+  },
+  {
+    "revision": "6bb2a4015cbf242ad2ad",
+    "url": "/js-notes/static/js/main.7cadc4d3.chunk.js"
+  },
+  {
+    "revision": "38b4aa7d3455933022ea",
+    "url": "/js-notes/static/js/runtime-main.200a0244.js"
+  },
+  {
+    "revision": "9e05b9c90b4da9590c7b2a5540b0d6fc",
+    "url": "/js-notes/static/media/presentation.9e05b9c9.png"
+  }
+]);
